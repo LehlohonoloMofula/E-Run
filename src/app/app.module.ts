47a0bf9d6b7089d939fa04f551035002b7d9cb97
Lehlohonolo <lehlohonolom@hopeinstitute.co.za>
2020-10-22 13:50:45 +0200

    added menu errand/detail nav componentd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ErrandsComponent } from './errands/errands.component';
+import { ErrandDetailsComponent } from './errands/errand-details/errand-details.component';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrandsComponent,
+    ErrandDetailsComponent,
+    AccountComponent,
+    HomeComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/errands/errand-details/errand-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-errand-details',
+  templateUrl: './errand-details.component.html',
+  styleUrls: ['./errand-details.component.css']
+})
+export class ErrandDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
