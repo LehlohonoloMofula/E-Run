1d0933fce42d3fbab99296a682bef957b57b57e8
Lehlohonolo <lehlohonolom@hopeinstitute.co.za>
2020-10-23 17:07:13 +0200

    just random stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
 
+import { AccountComponent } from './account/account.component';
 import { AppComponent } from './app.component';
 import { ErrandsComponent } from './errands/errands.component';
 import { ErrandDetailsComponent } from './errands/errand-details/errand-details.component';
-import { AccountComponent } from './account/account.component';
+import { Error404Component } from './error404/error404.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ErrandCreateComponent } from './errands/errand-create/errand-create.component';
 
 @NgModule({
   declarations: [
+    AccountComponent,
     AppComponent,
     ErrandsComponent,
     ErrandDetailsComponent,
-    AccountComponent,
+    Error404Component,
     HomeComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    ErrandCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'errands', component: ErrandsComponent }
+      // add redirection
+      //  { path: 'error404', component: Error404Component }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
